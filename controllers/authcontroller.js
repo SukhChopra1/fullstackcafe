59d37de7711f04ad61c457c5e0f63b099cb5b7d8
SukhChopra1 <choprasukh304@gmail.com>
2026-04-06 11:35:02 +0530

    Hello!

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const Cart = require("../models/Cart");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
