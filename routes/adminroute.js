a01a55030bb0774bba8807f90375a4a0eaf21c99
SukhChopra1 <choprasukh304@gmail.com>
2026-04-06 11:44:11 +0530

    Hellonpm run dev

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admincontroller");
-const { requireAuth } = require("../middleware/authMiddleware");
+const { requireAuth } = require("../middleware/authmiddleware");
 const upload = require("../middleware/multer");
 
 /* ================= ADMIN CHECK MIDDLEWARE ================= */
